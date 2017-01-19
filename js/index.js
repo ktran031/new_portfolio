7c705f4ae67eda6a59946522c5ab0db140193f13
ktran031 <ktran031@gmail.com>
2017-01-19 15:38:35 -0800

    f12

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+
+
+$(window).scroll(function(){
+    $(".jumbotron").css("opacity", 1 - $(window).scrollTop() / 250);
+  });
+
     
   $("a#inline").fancybox({
         'hideOnContentClick': true
@@ -36,6 +42,21 @@ $(document).ready(function(){
         }
     });
   });
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+
 })
 
 
